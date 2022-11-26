aea3d491869bac9a454e0c391076c1f22a9d39b6
Fabio Alexandre <famhs@ecomp.poli.br>
2022-11-26 13:12:56 -0400

    attempt

--- hooks/push.ts
+++ hooks/push.ts
@@ -5,13 +5,17 @@ export const usePush = (uri: string, method = "POST") => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState({});
 
-  const pushData = async (data) => {
+  const pushData = async (data, attempt = 10) => {
     setIsLoading(true);
     let response;
     if (method === "POST") {
       response = await post(uri, data);
     } else {
       response = await put(uri, data);
+    }
+    if (response.status === 503 && attempt >= 0) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return await pushData(data, attempt - 1);
     }
     setIsLoading(false);
     const result = response.data;
